5f30d0951104ddf4b40751f7dbc830e26126e0fe
RafaPalau <rafaeldonizetip@gmail.com>
2021-07-01 11:36:10 -0300

    responsive Header component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,19 @@
 
 import { Box, Flex, Text , Avatar} from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps{
+  showProfileData?: boolean
+}
+
+export function Profile({showProfileData} : ProfileProps){
   return (
     <Flex align="center">
-    <Box mr="4" textAlign="right">
+   { showProfileData && (
+      <Box mr="4" textAlign="right">
       <Text>Rafael Palau</Text>
       <Text color="gray.300" fontSize="small"> rafaeldonizetip@gmail.com</Text>
     </Box>
+   )}
     <Avatar size="md" name="Rafael Palau" src="https://github.com/rafapalau.png" />
   </Flex>
   )
